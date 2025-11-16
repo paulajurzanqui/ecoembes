a2cd3ec8c9724891fa279c688182e3c19c2a8f91
Paula Jiménez Urzanqui <paula.j.urzanqui@opendeusto.es>
2025-11-16 17:35:17 +0100

    Update AuthController con DTO

--- src/main/java/es/deusto/sd/auctions/facade/AuthController.java
+++ src/main/java/es/deusto/sd/auctions/facade/AuthController.java
@@ -6,6 +6,7 @@
 package es.deusto.sd.auctions.facade;
 
 import java.util.Optional;
+import es.deusto.sd.auctions.dto.CredentialsDTO;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,54 +24,54 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/auth")
 @Tag(name = "Authorization Controller", description = "Login and logout operations")
 public class AuthController {
-/*
-    private AuthService authService;
-    
-	/*public AuthController(AuthService authService) {
+	
+	private AuthService authService;
+
+	public AuthController(AuthService authService) {
 		this.authService = authService;
 	}
 
-    // Login endpoint
-    @Operation(
-        summary = "Login to the system",
-        description = "Allows a user to login by providing email and password. Returns a token if successful.",
-        responses = {
-            @ApiResponse(responseCode = "200", description = "OK: Login successful, returns a token"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized: Invalid credentials, login failed"),
-        }
-    )
-    @PostMapping("/login")
-    public ResponseEntity<String> login(
-    		@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "User's credentials", required = true)
-    		@RequestBody CredentialsDTO credentials) {    	
-        Optional<String> token = authService.login(credentials.getEmail(), credentials.getPassword());
-        
-    	if (token.isPresent()) {
-    		return new ResponseEntity<>(token.get(), HttpStatus.OK);
-    	} else {
-    		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    	}
-    }
-    // Logout endpoint
-    @Operation(
-        summary = "Logout from the system",
-        description = "Allows a user to logout by providing the authorization token.",
-        responses = {
-            @ApiResponse(responseCode = "204", description = "No Content: Logout successful"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized: Invalid token, logout failed"),
-        }
-    )    
-    @PostMapping("/logout")    
-    public ResponseEntity<Void> logout(
-    		@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Authorization token in plain text", required = true)
-    		@RequestBody String token) {    	
-        Optional<Boolean> result = authService.logout(token);
-    	
-        if (result.isPresent() && result.get()) {
-        	return new ResponseEntity<>(HttpStatus.NO_CONTENT);	
-        } else {
-        	return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }        
-    }*/
+	// Login endpoint
+	@Operation(
+		summary = "Login to the system",
+		description = "Allows a user to login by providing email and password. Returns a token if successful.",
+		responses = {
+			@ApiResponse(responseCode = "200", description = "OK: Login successful, returns a token"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized: Invalid credentials, login failed"),
+		}
+	)
+	@PostMapping("/login")
+	public ResponseEntity<String> login(
+			@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "User's credentials", required = true)
+			@RequestBody CredentialsDTO credentials) {
+		Optional<String> token = authService.login(credentials.getEmail(), credentials.getPassword());
 
+		if (token.isPresent()) {
+			return new ResponseEntity<>(token.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
+	}
+	
+	// Logout endpoint
+	@Operation(
+		summary = "Logout from the system",
+		description = "Allows a user to logout by providing the authorization token.",
+		responses = {
+			@ApiResponse(responseCode = "204", description = "No Content: Logout successful"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized: Invalid token, logout failed"),
+		}
+	)
+	@PostMapping("/logout")
+	public ResponseEntity<Void> logout(
+			@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Authorization token in plain text", required = true)
+			@RequestBody String token) {
+		Optional<Boolean> result = authService.logout(token);
+
+		if (result.isPresent() && result.get()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
+	}
 }
